6d5d6f514997792afce738e2641be5ec9e2ffe85
Hao SUN <hsun-code@outlook.com>
2022-10-31 09:50:05 +0000

    package: install basic packages

---
+++ packages/install_basic_packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# returns 1 if the package was already installed and 0 otherwise.
+# The first argument is the package name to be checked (and installed if not already).
+# other arguments are passed to apt-get
+try_install() {
+    dpkg -l "$1" | grep -q ^ii && msg "Installed already" && return 1
+    sudo apt-get -y install "$@"
+    return 0
+}
+
+banner "Try to install package tmux"
+try_install tmux
+
