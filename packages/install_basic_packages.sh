3481fdfbed93ab5adec7812ad98db70f415b25ef
Hao SUN <hsun-code@outlook.com>
2022-11-01 03:01:30 +0000

    packages: add OpenJDK dependencies

--- packages/install_basic_packages.sh
+++ packages/install_basic_packages.sh
@@ -4,7 +4,7 @@
 # The first argument is the package name to be checked (and installed if not already).
 # other arguments are passed to apt-get
 try_install() {
-    dpkg -l "$1" | grep -q ^ii && msg "Installed already" && return 1
+    dpkg -l "$1" | grep -q ^ii && msg "$1: Installed already" && return 1
     sudo apt-get -y install "$@"
     return 0
 }
@@ -14,4 +14,16 @@ try_install tmux
 
 banner "Try to install package safe-rm"
 try_install safe-rm
+
+# JDK dependencies
+banner "Try to install OpenJDK dependencies"
+try_install libasound2-dev
+try_install libcups2-dev
+try_install libfontconfig1-dev
+try_install libx11-dev
+try_install libxext-dev
+try_install libxrender-dev
+try_install libxrandr-dev
+try_install libxtst-dev
+try_install libxt-dev
 
