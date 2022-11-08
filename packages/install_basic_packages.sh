568f004df7c4647e5d037ae1f978bf4524a79041
Hao Sun <hsun-code@outlook.com>
2022-11-08 01:55:29 +0000

    packages: move nvim package install into the basic packages scope
    install nodejs 14 for nvim

--- packages/install_basic_packages.sh
+++ packages/install_basic_packages.sh
@@ -9,6 +9,14 @@ try_install() {
     return 0
 }
 
+# returns 1 if the package was already installed and 0 otherwise.
+is_installed() {
+    dpkg -l "$1" | grep -q ^ii && msg "Installed already" && return 1
+    return 0
+}
+
+#######################################
+
 banner "Try to install package tmux"
 try_install tmux
 
@@ -29,4 +37,35 @@ try_install libxrender-dev
 try_install libxrandr-dev
 try_install libxtst-dev
 try_install libxt-dev
+
+# Neovim
+banner "Try to install package neovim"
+is_installed neovim
+if [[ $? -eq 0 ]];  then
+  # https://github.com/neovim/neovim/wiki/Installing-Neovim
+  sudo add-apt-repository ppa:neovim-ppa/stable
+  sudo apt-get update
+  sudo apt-get install -y neovim
+fi
+
+banner "Try to install Neovim dependencies"
+
+msg "Try to install package Packer"
+packer_dir="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim/"
+if [[ -d "$packer_dir" ]]; then
+  msg "Installed already"
+else
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
+  msg "Done"
+fi
+
+msg "Try to install package nodejs 14"
+is_installed nodejs
+if [[ $? -eq 0 ]];  then
+  curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+  sudo apt-get update
+  sudo apt-get install -y nodejs
+fi
+
+# try_install npm
 
