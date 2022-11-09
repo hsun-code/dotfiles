ea8a6c4c6cd04400944dde71886ebcce7c564710
Hao Sun <hsun-code@outlook.com>
2022-11-09 07:29:37 +0000

    neovim: add NvChad

--- packages/install_neovim.sh
+++ packages/install_neovim.sh
@@ -11,7 +11,7 @@ if [[ -f $nvim_bin ]]; then
     msg "Has neovim, but not v0.8.0"
     need_install=true
   else
-    msg "Already installed"
+    msg "Installed already"
   fi
 else
   msg "No neovim"
@@ -35,5 +35,22 @@ if [[ "$need_install" = true ]]; then
   rm -rf $nvim_src 
   $nvim_bin -v
   msg "Success"
+fi
+
+banner "Try to install NvChad's prerequisites"
+# Required for grep searching with Telescope plugin
+hsun_try_install ripgrep
+
+banner "Try to install NvChad"
+# TODO: will change to my forked repo later.
+# TODO: in the bashrc, will pull the latest code each time
+nvchad_repo="https://github.com/NvChad/NvChad"
+nvchad_dir="$HOME/.config/nvim"
+if [[ ! -d $nvchad_dir ]]; then
+  rm -rf ~/.local/share/nvim
+  git clone $nvchad_repo $nvchad_dir --depth 1
+  msg "Done"
+else
+  msg "Installed already"
 fi
 
