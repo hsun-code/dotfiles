e055e37f53fe8e469de111d3c34223c30479322c
Hao Sun <hsun-code@outlook.com>
2022-11-09 07:03:44 +0000

    packages: add helper

---
+++ packages/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source helper.sh
+bash install_basic_packages.sh
+bash install_neovim.sh
+
--- packages/install_basic_packages.sh
+++ packages/install_basic_packages.sh
@@ -1,43 +1,26 @@
 #!/usr/bin/env bash
 
-# returns 1 if the package was already installed and 0 otherwise.
-# The first argument is the package name to be checked (and installed if not already).
-# other arguments are passed to apt-get
-try_install() {
-    dpkg -l "$1" | grep -q ^ii && msg "$1: Installed already" && return 1
-    sudo apt-get -y install "$@"
-    return 0
-}
-
-# returns 1 if the package was already installed and 0 otherwise.
-is_installed() {
-    dpkg -l "$1" | grep -q ^ii && msg "Installed already" && return 1
-    return 0
-}
-
-#######################################
-
 banner "Try to install package tmux"
-try_install tmux
+hsun_try_install tmux
 
 banner "Try to install package safe-rm"
-try_install safe-rm
+hsun_try_install safe-rm
 
 banner "Try to install package numactl"
-try_install numactl
+hsun_try_install numactl
 
 banner "Try to install package tree"
-try_install tree
+hsun_try_install tree
 
 # JDK dependencies
 banner "Try to install OpenJDK dependencies"
-try_install libasound2-dev
-try_install libcups2-dev
-try_install libfontconfig1-dev
-try_install libx11-dev
-try_install libxext-dev
-try_install libxrender-dev
-try_install libxrandr-dev
-try_install libxtst-dev
-try_install libxt-dev
+hsun_try_install libasound2-dev
+hsun_try_install libcups2-dev
+hsun_try_install libfontconfig1-dev
+hsun_try_install libx11-dev
+hsun_try_install libxext-dev
+hsun_try_install libxrender-dev
+hsun_try_install libxrandr-dev
+hsun_try_install libxtst-dev
+hsun_try_install libxt-dev
 
