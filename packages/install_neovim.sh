ca85886086af4dc91e66f5d97bf16adafa0375e4
Hao Sun <hsun-code@outlook.com>
2023-06-07 02:11:08 +0000

    install neovim: add uninstall process and support install version selection

--- packages/install_neovim.sh
+++ packages/install_neovim.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-banner "Try to install package neovim v0.8.0"
+# Reference: https://github.com/neovim/neovim/wiki/Installing-Neovim#install-from-source
 
+# Note-1: we assume that if there exists one neovim installed already,
+# it should be installed in the same way. Otherwise, we may not find the
+# expected binary and I'm afraid the uninstall process doesn't work either.
+
+banner "Try to install package neovim"
+
+nvim_git_url="https://github.com/neovim/neovim.git"
+nvim_version="0.9"
+nvim_branch="release-${nvim_version}"
 nvim_bin="/usr/local/bin/nvim"
 need_install=false
+need_uninstall=false
 
+#
+# Check if there exists the neovim we want to install
+#
 if [[ -f $nvim_bin ]]; then 
-  $nvim_bin -v | grep "v0.8.0"
+  $nvim_bin -v | grep "NVIM v${nvim_version}"
   if [[ $? == 1 ]]; then
-    msg "Has neovim, but not v0.8.0"
+    msg "Has neovim, but not v${nvim_version}"
     need_install=true
+    need_uninstall=true
   else
     msg "Installed already"
   fi
@@ -18,26 +32,45 @@ else
   need_install=true
 fi
 
+#
+# Uninstall the existing neovim, which is not our target version
+#
+if [[ "$need_uninstall" = true ]]; then
+  # Reference https://github.com/neovim/neovim/wiki/Installing-Neovim#uninstall
+  msg "Uninstall old neovim"
+  sudo rm $nvim_bin
+  sudo rm -r "/usr/local/share/nvim/"
+  msg "Uninstall finish"
+# TODO: remove more temp data?
+# rm -rf ~/.config/nvim
+# rm -rf ~/.local/share/nvim
+fi
+
+#
+# Install the neovim we want to install
+#
 if [[ "$need_install" = true ]]; then
-  banner "Build prerequisites"
+  msg "Build prerequisites"
   sudo apt-get install ninja-build gettext libtool libtool-bin autoconf \
        automake cmake g++ pkg-config unzip curl doxygen
 
-  nvim_src=/tmp/nvim-0.8
-  banner "Download the source code into $nvim_src"
-  git clone --branch release-0.8 --depth 1 \
-    https://github.com/neovim/neovim.git $nvim_src
+  nvim_src="/tmp/nvim_${RANDOM}"
+  msg "Download the source code into $nvim_src"
+  git clone --branch $nvim_branch --depth 1 $nvim_git_url $nvim_src
 
-  banner "Install neovim"
+  msg "Install neovim"
   cd $nvim_src; make CMAKE_BUILD_TYPE=Release
   sudo make install
   cd -
   rm -rf $nvim_src 
   $nvim_bin -v
-  msg "Success"
+  msg "Install finish"
 fi
 
+#
+# Install NvChad's prerequisites
+#
 banner "Try to install NvChad's prerequisites"
-# Required for grep searching with Telescope plugin
+msg "install ripgrep: Required for grep searching with Telescope plugin"
 hsun_try_install ripgrep
 
