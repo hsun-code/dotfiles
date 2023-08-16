83bf97fe62f21a906b67f2a65f2556e97fe6de2a
Hao Sun <hsun-code@outlook.com>
2023-08-16 15:00:38 +0000

    macos: install common packages

--- packages/install.sh
+++ packages/install.sh
@@ -4,6 +4,14 @@ source $HOME/hsun-code.code/dotfiles/packages/helper.sh
 
 if is_macos; then
   hsun_macos_try_install bash-completion
+  # Install common utilities.
+  # Install coreutils for shell 'realpath command not found' issue.
+  brew install vim wget curl git tree procmail jq ed findutils gawk grep make bash
+  brew install gnutls gnu-sed gnu-tar gnu-indent gnu-getopt coreutils openldap autoconf
+  brew install binutils diffutils gnu-which gzip screen watch wdiff gpatch less
+  brew install openssh perl rsync unzip file-formula bash-completion mysql-client@8.0
+  # Install dot
+  brew install graphviz
 else
   bash $HSUN_DOTS/packages/install_basic_packages.sh
 fi
